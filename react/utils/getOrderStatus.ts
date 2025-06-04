22aeccc857bbaa69a048b982384917379dabe026
William Di Biasi Bogik <williambogik@gmail.com>
2025-06-04 19:25:57 -0300

    Refactor getOrderStatus for readability and consistency

--- react/utils/getOrderStatus.ts
+++ react/utils/getOrderStatus.ts
@@ -1,4 +1,4 @@
-import { OrderListItemWithDetails } from '../../node/types/orderList'
+import type { OrderListItemWithDetails } from '../../node/types/orderList'
 import { formatDate } from './formats'
 
 export const getOrderStatus = (
@@ -14,20 +14,28 @@ export const getOrderStatus = (
   switch (status.toLowerCase()) {
     case 'payment-pending':
       return { label: 'Pagamento pendente', variant: 'warning', tooltip: '' }
+
     case 'cancellation-requested':
       return { label: 'Cancelamento solicitado', variant: 'warning', tooltip: '' }
+
     case 'canceled':
       return { label: 'Cancelado', variant: 'destructive', tooltip: formatDate(cancellationDate) }
+
     case 'invoiced':
       return { label: 'Faturado', variant: 'default', tooltip: formatDate(invoicedDate) }
+
     case 'ready-for-handling':
       return { label: 'Pronto para envio', variant: 'outline', tooltip: '' }
+
     case 'handling':
       return { label: 'Preparando pedido', variant: 'success', tooltip: '' }
+
     case 'shipped':
       return { label: 'Enviando pedido', variant: 'success', tooltip: '' }
+
     case 'delivered':
       return { label: 'Entregue', variant: 'default', tooltip: '' }
+
     default:
       return { label: status, variant: 'outline', tooltip: '' }
   }
